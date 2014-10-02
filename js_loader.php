9eefbc08ed7210c0c9419a8a8e6dd9db350f93fa
Bernardus Billy Tjiptoning <bernardusbilly@Bernarduss-MacBook-Pro.local>
2014-10-01 17:50:39 -0700

    Revert 16afbbd..6ae4093
    This rolls back to commit 16afbbd0c520305bc4236f670fc2bf1509c56c4d.

--- js_loader.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="./resources/js/jquery-1.11.0.js"></script>
-<script type="text/javascript" src="./resources/js/style.js"></script>
-<script type="text/javascript" src="./resources/js/google-analytics.js"></script>
