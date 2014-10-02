65193a488b5fcaef782b1c176813110085990c52
Bernardus Billy Tjiptoning <bernardusbilly@Bernarduss-MacBook-Pro.local>
2014-10-01 17:33:47 -0700

    Test
    - added projects.php

---
+++ projects.php
@@ -0,0 +1,132 @@
+<!DOCTYPE php>
+<!-- resume.php -->
+<html lang="en">
+<head>
+	<title>Bernardus Billy Tjiptoning</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	<link rel="icon" type="image/png" href="http://bernardusbilly.com/favicon.png">
+	
+	<?php include 'css_loader.php' ?>
+</head>
+<body>
+	<div id="header">
+		<div class="container">
+			<div class="row">
+				<div id="name" class="col-lg-5 col-md-6 col-sm-5 col-xs-12">
+					Bernardus Billy Tjiptoning
+				</div>
+				<div id="menu-container" class="col-lg-5 col-lg-offset-2 col-md-6 col-sm-7 col-xs-12">
+					<a href="http://bernardusbilly.com" class="menu"><span class="glyphicon glyphicon-home" style="padding-right: 5px;"></span>Home</a>
+					<a href="./resume.html" class="menu"><span class="glyphicon glyphicon-list-alt" style="padding-right: 5px;"></span>Resume</a>
+					<a href="./projects.html" class="menu menu-active"><span class="glyphicon glyphicon-folder-open" style="padding-right: 5px;"></span>Projects</a>
+					<a href="./contact.html" class="menu"><span class="glyphicon glyphicon-earphone" style="padding-right: 5px;"></span>Contact</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="main">
+		<div class="container">
+			<div class="entry">
+				<div class="h2">Projects</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">Peeplogic<a href="http://peeplogic.com" target="_blank"><span class="glyphicon glyphicon-link" style="padding-left: 10px;"></span></a></div>
+							An ongoing project by myself with the main idea of creating a page with list of things that people really need during busy hours; such as: calendar, time, and list of things they need to do where they can decide which of the lists they want to share with their friends.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+							<a href="./resources/img/preview/peeplogic.png" target="_blank"><img src="./resources/img/preview/peeplogic.png" class="preview"></a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">IEC Transport App<a href="http://local-alignment-675.appspot.com/" target="_blank"><span class="glyphicon glyphicon-link" style="padding-left: 10px;"></span></a></div>
+							An ongoing project by IEC San Jose to make the Transportation Ministry easier to manage the people who are in need to be picked up to the church's event.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+							<a href="./resources/img/preview/iects.png" target="_blank"><img src="./resources/img/preview/iects.png" class="preview"></a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">Processor Design</div>
+							Implement a 2-stage pipelined processor for the Ida assembly language - similar to MIPS assembly language and use logisim as the primary software. The processor has 16 registers with each has 24 bits wide and each of them has its own convention. NOTE: image is blurred on purposed - to prevent any copy as it is part of class project's.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+							<a href="./resources/img/preview/logisim.png" target="_blank"><img src="./resources/img/preview/logisim.png" class="preview"></a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">2048<a href="http://goo.gl/xaqboh" target="_blank"><span class="glyphicon glyphicon-link" style="padding-left: 10px;"></span></a></div>
+							A replica of the 2048 game without the User Interface. In progress of making the AI to achieve the highest score possible using expectiminimax algorithm.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+							<a href="./resources/img/preview/2048.png" target="_blank"><img src="./resources/img/preview/2048.png" class="preview"></a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">Twitter Trends</div>
+							Develop a geographic visualization of Twitter data across the USA. The output map referenced from public tweets of a specific trend that have been tagged with geographic locations. Each tweet is assigned to a sentiment based on all the words it contains. Aggregate tweets by the state with the closest geographic center and finally give a color representation to each state to the aggregate sentiment of its tweets.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+							<a href="./resources/img/preview/twitterTrends.png" target="_blank"><img src="./resources/img/preview/twitterTrends.png" class="preview"></a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">Take Coins Game<a href="http://goo.gl/06QL0w" target="_blank"><span class="glyphicon glyphicon-link" style="padding-left: 10px;"></span></a></div>
+							Implemented one of the Game Theory called the Take-Away game. Combining the knowledge of python and using the library extension called pygame, I developed a simple User Interface for the game. NOTE: pygame must be installed to play this game.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+							<a href="./resources/img/preview/takeCoinsGame.png" target="_blank"><img src="./resources/img/preview/takeCoinsGame.png" class="preview"></a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">Random Security Password Generator<a href="http://goo.gl/kvD4lB" target="_blank"><span class="glyphicon glyphicon-link" style="padding-left: 10px;"></span></a></div>
+							A password checker that can also be used for password generator for a specific password that changes according to current date and time when the password is generated. Using that information, it graphs a function, calculate the maximum and minimum point to be a number to shift original password - similar to bit shift.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-11">
+							<div class="item-header">Film Editor and Cinematographer<a href="http://goo.gl/LldyAQ" target="_blank"><span class="glyphicon glyphicon-link" style="padding-left: 10px;"></span></a></div>
+							Collection of promotional video for nonprofit organization such as IFGF Pinole and IEC San Jose.
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
+							<a href="./resources/img/preview/youtube.png" target="_blank"><img src="./resources/img/preview/youtube.png" class="preview"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container">
+			Copyright &copy 2014 Bernardus Billy Tjiptoning
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="./resources/js/jquery-1.11.0.js"></script>
+<script type="text/javascript" src="./resources/js/style.js"></script>
+<script type="text/javascript" src="./resources/js/google-analytics.js"></script>
+</html>
