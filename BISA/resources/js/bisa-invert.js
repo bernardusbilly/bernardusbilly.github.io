c78d57d37bae0e39bb965f94451378b2a5dc14d6
Bernardus Billy Tjiptoning <bernardusbilly@Bernarduss-MacBook-Pro.local>
2014-10-12 12:01:35 -0700

    Update
    - added invert (color) example for BISA homepage

---
+++ BISA/resources/js/bisa-invert.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	console.log("BISA.js ready");
+	
+	var names = $('#photoset-frame > img');
+
+	$(names).each(function() {
+		var tmp = $(this).attr('id');
+		var name = tmp.substr(0, tmp.indexOf('-'));
+		var shift_right = $(this).attr('shift');
+		$(this).hasClass('photo')
+
+		if($(this).hasClass('photo')) {
+			$(this).hover(function() {
+				$('#' + name + '-info').css("left", shift_right+"%");
+				$('#photoset-info').css("border-left", "3px solid black");
+				$('#background').attr("src", "resources/img/bisa-bw.jpg");
+			}, function() {
+				$('#' + name + '-info').css("left", "-200%");
+				$('#photoset-info').css("border-left", "0px");
+				$('#background').attr("src", "resources/img/bisa.jpg");
+			});
+		}
+	});
+
+	// shorten the Berkeley Indonesian Student Association to BISA when in mobile-version
+
+	if ($(window).width() < 441) {
+		$('#header .navbar-brand').html("BISA");
+	} else {
+		$('#header .navbar-brand').html("Berkeley Indonesian Student Association");
+	}
+		
+	$(window).resize(function() {
+		if ($(window).width() < 441) {
+			$('#header .navbar-brand').html("BISA");
+		} else {
+			$('#header .navbar-brand').html("Berkeley Indonesian Student Association");
+		}
+	});
+});
