662bfae8dce01b4339ab4dc21c3d796665d5fe2a
Bernardus Billy Tjiptoning <bernardusbilly@Bernarduss-MacBook-Pro.local>
2014-10-13 22:17:22 -0700

    Update
    - update compatibility with firefox and safari

--- BISA/resources/js/bisa.js
+++ BISA/resources/js/bisa.js
@@ -13,13 +13,21 @@ $(document).ready(function() {
 			$(this).hover(function() {
 				$('#' + name + '-info').css("left", shift_right+"%");
 				$('#photoset-info').css("border-left", "3px solid black");
-				$('#background').css("opacity", "0");
+				$('#background').css("opacity", 0);
+				$('#background').css("-moz-opacity", 0);
+				$('#background').css("-khtml-opacity", 0);
 				$('#background-bw').css("opacity", "1");
+				$('#background-bw').css("-moz-opacity", 0);
+				$('#background-bw').css("-khtml-opacity", 0);
 			}, function() {
 				$('#' + name + '-info').css("left", "-200%");
 				$('#photoset-info').css("border-left", "0px");
 				$('#background').css("opacity", "1");
-				$('#background-bw').css("opacity", "0");
+				$('#background').css("-moz-opacity", 1);
+				$('#background').css("-khtml-opacity", 1);
+				$('#background-bw').css("opacity", 0);
+				$('#background-bw').css("-moz-opacity", 0);
+				$('#background-bw').css("-khtml-opacity", 0);
 			});
 		}
 	});
