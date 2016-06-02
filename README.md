d165fba614cc406a7dc271c3e6f815a2ea7dd193
bernardusbilly <bernardus.billy@hotmail.com>
2016-06-01 22:59:03 -0700

    Change the whole layout to the new look

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-### Personal Website
-This repository holds Billy's personal website.
-
-### Link
-http://www.bernardusbilly.com
+# tjiptoning
+Tjiptoning Sejati 
---
+++ assets/js/scripts.js
@@ -0,0 +1,64 @@
+$(window).load(function() {
+	$('.loading-wrapper').hide();
+	$('.menu-container').addClass("animated fadeInRight");
+  $('.logo').animate({
+      width: "250px"
+  }, 800, "easeOutBounce");
+});
+
+$('.wrapper-project').hover(function() {
+	$(this).find(".project-preview").css("opacity", "1");
+}, function() {
+	$(this).find(".project-preview").css("opacity", "0");
+})
+
+$("ul>li>a").click(function(e) {
+	var target = $(this).attr("href");
+	e.preventDefault();
+	console.log(target);
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 1000, 'easeInOutQuint');
+});
+
+var service = $('.services').waypoint({
+  handler: function() {
+  	$('.services li').css("visibility", "visible");
+    $('.services li').addClass("animated fadeInRight");
+  },
+  offset: "70%"
+})
+
+var yearIndicator = $('.year-indicator').waypoint({
+  handler: function(direction) {
+  	$(this.element).css("visibility", "visible");
+    $(this.element).addClass("animated fadeInDown");
+  },
+  offset: "70%"
+})
+
+var leftHistory = $('.wrapper-left').waypoint({
+  handler: function(direction) {
+  	$(this.element).css("visibility", "visible");
+    $(this.element).addClass("animated fadeInLeft");
+  },
+  offset: "70%"
+})
+
+var rightHistory = $('.wrapper-right').waypoint({
+  handler: function(direction) {
+  	$(this.element).css("visibility", "visible");
+    $(this.element).addClass("animated fadeInRight");
+  },
+  offset: "70%"
+})
+
+var projectHeader = $('.wrapper-project .project-header').waypoint({
+  handler: function(direction) {
+  	$(this.element).css("visibility", "visible");
+    $(this.element).addClass("animated fadeInLeft");
+    $(this.element).next().css("visibility", "visible");
+    $(this.element).next().addClass("animated fadeInLeft");
+  },
+  offset: "90%"
+})
